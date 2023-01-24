d96f394084202bb3611e41cb6cb5b62530e10bb0
JBlay10 <joseblay10@gmail.com>
2023-01-23 18:30:19 -0600

    Test backend query and mutations

--- server/server.js
+++ server/server.js
@@ -11,13 +11,15 @@ const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
